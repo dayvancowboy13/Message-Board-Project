9658e9cb109b9f5c63b0d2c560d6ee9a92bea839
dayvancowboy13 <chris.e.friedrich@gmail.com>
2025-07-26 10:07:03 -0700

    Add index template, setup ejs in app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('node:path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 
 const indexRouter = require('./routes/indexRouter');
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,19 @@ const { Router } = require('express');
 
 const indexRouter = Router();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hellow world!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
 indexRouter.get('/', (req, res) => {
     res.send('You are accessing the home page!')
 });
