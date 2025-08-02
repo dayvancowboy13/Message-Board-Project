8d5e9a1fa5b85850d1bd432353442e57d7cedbfa
dayvancowboy13 <chris.e.friedrich@gmail.com>
2025-08-01 19:43:10 -0700

    Typo -_-

--- db/queries.js
+++ db/queries.js
@@ -8,7 +8,7 @@ exports.getAll = async function () {
 
 exports.insertMessage = async function (author, message) {
     await pool.query(`INSERT INTO messages (author, message, date_added) 
-        VALUES ($!1, $2, CURRENT_TIMESTAMP)`, [author, message])
+        VALUES ($1, $2, CURRENT_TIMESTAMP)`, [author, message])
 
 }
 
